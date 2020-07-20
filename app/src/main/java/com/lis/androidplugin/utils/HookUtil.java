73b4bcb90668d947618f71bd7bc902c8de0e80f5
lishuai <lishuai1@leyou.com>
2020-07-20 18:24:25 +0800

    适配23，26及以上版本问题。

--- app/src/main/java/com/lis/androidplugin/utils/HookUtil.java
+++ app/src/main/java/com/lis/androidplugin/utils/HookUtil.java
@@ -1,6 +1,7 @@
 package com.lis.androidplugin.utils;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
@@ -9,6 +10,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.List;
 
 /**
  * Created by lis on 2020/7/20.
@@ -18,11 +20,19 @@ public class HookUtil {
 
     public static void hookAMS() {
         try {
-            //获取Singleton对象
-            Class<?> aClass = Class.forName("android.app.ActivityManager");
-            Field singletonField = aClass.getDeclaredField("IActivityManagerSingleton");
-            singletonField.setAccessible(true);
-            Object singleton = singletonField.get(null);
+            Object singleton = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                //获取Singleton对象
+                Class<?> aClass = Class.forName("android.app.ActivityManager");
+                Field singletonField = aClass.getDeclaredField("IActivityManagerSingleton");
+                singletonField.setAccessible(true);
+                singleton = singletonField.get(null);
+            } else {
+                Class<?> aClass = Class.forName("android.app.ActivityManagerNative");
+                Field singletonField = aClass.getDeclaredField("gDefault");
+                singletonField.setAccessible(true);
+                singleton = singletonField.get(null);
+            }
             //获取 IActivityManager对象
             Class<?> singletonClass = Class.forName("android.util.Singleton");
             Field mInstanceField = singletonClass.getDeclaredField("mInstance");
@@ -36,7 +46,6 @@ public class HookUtil {
                         @Override
                         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                             //IActivityManager的方法执行的时候，都会先跑这儿
-
                             //替换Intent
                             if ("startActivity".equals(method.getName())) {
                                 //替换Intent
@@ -55,8 +64,6 @@ public class HookUtil {
                                 //保存原来的
                                 proxyIntent.putExtra(TARGET_INTENT, intent);
                                 args[index] = proxyIntent;
-
-
                             }
                             //原来的方法
                             return method.invoke(mInstance, args);
@@ -71,6 +78,7 @@ public class HookUtil {
 
     public static void hookHandler() {
         try {
+
             final Class<?> aClass = Class.forName("android.app.ActivityThread");
             Field sCurrentActivityThreadField = aClass.getDeclaredField("sCurrentActivityThread");
             sCurrentActivityThreadField.setAccessible(true);
@@ -91,21 +99,11 @@ public class HookUtil {
 
                 @Override
                 public boolean handleMessage(Message msg) {
-                    Log.e("plugin", "handleMessage: what" + msg.what);
                     //将Intent替换回来
                     switch (msg.what) {
                         case 100:
                             //获取ActivityClientRecord中的intent对象
                             try {
-                                Log.e("plugin", "handleMessage: msg.obj" + msg.obj);
-//                                Field mActivitiesField = aClass.getDeclaredField("mActivities");
-//                                mActivitiesField.setAccessible(true);
-//                                ArrayMap<IBinder, Object> mActivities = (ArrayMap<IBinder, Object>) mActivitiesField.get(activityThread);
-//                                Object o = mActivities.get(msg.obj);
-//                                Field intentField = o.getClass().getDeclaredField("intent");
-//                                intentField.setAccessible(true);
-//                                Intent proxyIntent = (Intent) intentField.get(o);
-                                //获取ActivityClientRecord中的intent对象
                                 Field intentField = msg.obj.getClass().getDeclaredField("intent");
                                 intentField.setAccessible(true);
                                 Intent proxyIntent = (Intent) intentField.get(msg.obj);
@@ -115,10 +113,42 @@ public class HookUtil {
                                     Log.e("handler", intent.getComponent().getPackageName());
                                     Log.e("handler", intent.getComponent().getClassName());
                                     //替换回来
-                                    proxyIntent.setComponent(intent.getComponent());
+                                    //proxyIntent.setComponent(intent.getComponent());
+                                    //2替换
+                                    intentField.set(msg.obj, intent);
                                 }
                             } catch (Exception e) {
                                 Log.e("handler", e.getMessage());
+                                e.printStackTrace();
+                            }
+                            break;
+                        case 159:
+                            try {
+                                Class<?> clientTransaction = Class.forName("android.app.servertransaction.ClientTransaction");
+                                Field mActivityCallbacksField = clientTransaction.getDeclaredField("mActivityCallbacks");
+                                mActivityCallbacksField.setAccessible(true);
+                                List activityCallbacks = (List) mActivityCallbacksField.get(msg.obj);
+                                for (int i = 0; i < activityCallbacks.size(); i++) {
+                                    if (activityCallbacks.get(i).getClass().getName()
+                                            .equals("android.app.servertransaction.LaunchActivityItem")) {
+                                        Object launchItem = activityCallbacks.get(i);
+                                        Field mIntentField = launchItem.getClass().getDeclaredField("mIntent");
+                                        mIntentField.setAccessible(true);
+                                        Intent proxyIntent = (Intent) mIntentField.get(launchItem);
+                                        //拿到插件的Intent
+                                        Intent intent = proxyIntent.getParcelableExtra(TARGET_INTENT);
+                                        if (intent != null) {
+                                            Log.e("handler", intent.getComponent().getPackageName());
+                                            Log.e("handler", intent.getComponent().getClassName());
+                                            //替换回来
+                                            //proxyIntent.setComponent(intent.getComponent());
+                                            //2替换
+                                            mIntentField.set(launchItem, intent);
+                                        }
+                                    }
+                                }
+
+                            } catch (Exception e) {
                                 e.printStackTrace();
                             }
                             break;
